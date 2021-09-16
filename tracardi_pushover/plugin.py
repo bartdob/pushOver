33c3cc87717c5afe49d3d8fe76a8e1a6aa8d9175
Bartosz Dobrosielski <dobry1@pm.me>
2021-09-16 13:53:10 +0200

    ref

--- tracardi_pushover/plugin.py
+++ tracardi_pushover/plugin.py
@@ -13,22 +13,6 @@ class PushoverAction(ActionRunner):
     async def build(**kwargs) -> 'PushoverAction':
         plugin = PushoverAction(**kwargs)
         connection = Connection(**kwargs)
-        plugin.db = await connection.connect()
-
-        return plugin
-
-    def __init__(self, **kwargs):
-        self.db = None  # type: Optional[asyncpg.connection.Connection]
-        if 'message' not in kwargs:
-            raise ValueError("Please define message.")
-
-        self.message = kwargs['message']
-        self.timeout = kwargs['timeout'] if 'timeout' in kwargs else None
-
-    async def run(self, payload):
-        result = await self.db.fetch(self.message, timeout=self.timeout)
-        return Result(port="payload", value={"result": result})
-
 
 def register() -> Plugin:
     return Plugin(
--- tracardi_pushover/tests/test_plugin.py
+++ tracardi_pushover/tests/test_plugin.py
@@ -1,11 +1,11 @@
 import asyncio
 import os
-import urllib.parse
+load_dotenv()
 
 from tracardi_pushover.plugin import PushoverAction
 
 init = dict(
-    url="https://api.pushover.net/1/messages.json",
+    url='https://api.pushover.net/1/messages.json',
     token=os.getenv('APP_TOKEN'),
     user=os.getenv('USER_KEY'),
     message="Helllo"
@@ -16,8 +16,7 @@ payload = {}
 
 async def main():
     plugin = await PushoverAction.build(**init)
-    result = await plugin.run(payload)
-    print(plugin)
+
 
 
 asyncio.run(main())
